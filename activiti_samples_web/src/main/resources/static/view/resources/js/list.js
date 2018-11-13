bca858bceffda799f6864307f6baf854cc81c3f8
2507967396@qq.com <kali19960205@>
2018-11-13 10:44:36 +0800

    init msg

--- activiti_samples_web/src/main/resources/static/view/resources/js/list.js
+++ activiti_samples_web/src/main/resources/static/view/resources/js/list.js
@@ -1,50 +1,6 @@
+import APP from './vue/list.vue'
+
 new Vue({
     el: '#app',
-    data: function() {
-        return {
-            visible: false ,
-            tableData: [{
-                date: '2016-05-03',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-            }, {
-                date: '2016-05-02',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-            }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                province: '上海',
-                city: '普陀区',
-                address: '上海市普陀区金沙江路 1518 弄',
-                zip: 200333
-            }]
-        }
-    },
-    methods: {
-        tableRowClassName({row, rowIndex}) {
-            if (rowIndex === 1) {
-                return 'warning-row';
-            } else if (rowIndex === 3) {
-                return 'success-row';
-            }
-            return '';
-        },
-        handleClick(row) {
-            console.log(row);
-        }
-    }
+
 })
