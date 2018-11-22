658cc99042261cca13e3bdcb47d2d560418200dd
yywzt <2507967396@qq.com>
2018-11-22 23:00:23 +0800

    添加跨域配置

--- activiti_samples_provider/src/main/java/com/example/config/MyMvcConfig.java
+++ activiti_samples_provider/src/main/java/com/example/config/MyMvcConfig.java
@@ -1,6 +1,7 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,10 +20,19 @@ public class MyMvcConfig implements WebMvcConfigurer {
     }
 
     @Override
-
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/editor-app/**").addResourceLocations("classpath:/static/editor-app/");
         registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/view/resources/");
         registry.addResourceHandler("/html/**").addResourceLocations("classpath:/static/view/html/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081","null","*")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("Content-Type","X-Token")
+                .maxAge(3600L);
     }
 }
